10683f23ff05ec016aa9ab0819d1607b7bd23599
Henry <Henry@qq.com>
2020-06-11 20:56:41 +0800

    提交测试

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xu hengrui
+ * @create 2020-06-11 20:49
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
